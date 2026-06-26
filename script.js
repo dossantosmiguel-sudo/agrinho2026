a46d53f93d192272ae88e350dfa9eebcdc3c7211
dossantosmiguel-sudo <dos.santos.miguel@escola.pr.gov.br>
2026-06-26 17:58:17 -0300

    c

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ avanca.forEach(button => {
         document.getElementById(proximoPasso).classList.add('ativo');
     });
 });
+const guia = document.getElementById("guia");
+const btnGuia = document.getElementById("btnGuia");
+
+let guiaAtiva = false;
+
+btnGuia.onclick = () => {
+    guiaAtiva = !guiaAtiva;
+
+    guia.style.display = guiaAtiva ? "block" : "none";
+
+    btnGuia.textContent = guiaAtiva
+        ? "Desativar Guia"
+        : "Guia de Leitura";
+};
+
+document.addEventListener("mousemove",(e)=>{
+    if(guiaAtiva){
+        guia.style.top = (e.clientY-20)+"px";
+    }
+});
