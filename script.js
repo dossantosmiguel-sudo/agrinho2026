011064d1d950d9ba8ce18dcfdfc267545efd4cc2
dossantosmiguel-sudo <dos.santos.miguel@escola.pr.gov.br>
2026-06-26 22:26:58 -0300

    .

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ avanca.forEach(button => {
         document.getElementById(proximoPasso).classList.add('ativo');
     });
 });
+function boasVindas() {
+    const nome = document.getElementById('userName').value;
+    if (nome.trim() !== "") {
+        document.getElementById('greetingMessage').innerText = `Olá, ${nome}! Tudo bem? Seja muito bem vindo ao site do Miguel`;
+    } else
