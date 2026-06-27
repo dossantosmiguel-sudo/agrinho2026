6909c71a8bf486c0c9786b1601f9db58823fa23d
dossantosmiguel-sudo <dos.santos.miguel@escola.pr.gov.br>
2026-06-27 19:13:00 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(button => {
-    button.addEventListener('click', function () {
-
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
-
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
-    });
-});
 
 const btnSubmitName = document.getElementById('btn-submit-name');
 const inputName = document.getElementById('user-name');
