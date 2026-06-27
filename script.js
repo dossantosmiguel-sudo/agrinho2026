10791560bc132e435b0f6839605f73c383c9ff8d
dossantosmiguel-sudo <dos.santos.miguel@escola.pr.gov.br>
2026-06-27 18:36:14 -0300

    nome

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ avanca.forEach(button => {
         document.getElementById(proximoPasso).classList.add('ativo');
     });
 });
+const btnSubmitName = document.getElementById('btn-submit-name');
+const inputName = document.getElementById('user-name');
+const dynamicResponseDiv = document.getElementById('dynamic-response');
+const welcomeMessage = document.getElementById('welcome-message');
+const btnReady = document.getElementById('btn-ready');
+const contentSection = document.getElementById('content-section');
+const toggleThemeBtn = document.getElementById('toggle-theme-btn');
+
+
+btnSubmitName.addEventListener('click', () => {
+
+    const userName = inputName.value.trim();
+
+    if (userName !== "") {
+    
+        welcomeMessage.innerHTML = `Olá <strong>${userName}</strong>, seja bem vindo, vamos descobrir informações sobre dispositivos tecnológicos no agro.;
+        
+        dynamicResponseDiv.classList.remove('hidden-element');
+    } else {
+        alert("Digite o nome antes de enviar.");
+    }
+});
+
+btnReady.addEventListener('click', () => {
+
+    contentSection.classList.remove('hidden-element');
+});
